bf80eff08ed96424aff45b7de9d68e09ca560901
Scott Fitz <doctorwidget@gmail.com>
2024-07-21 10:44:22 -0700

    separate donut demo

--- src/components/renderers/SplitDuration.vue
+++ src/components/renderers/SplitDuration.vue
@@ -43,6 +43,7 @@ import {
  } from 'date-fns';
 
 import type { Chron } from '../../data/chrontypes';
+import DonutDemo from '../routes/DonutDemo.vue';
 
 const props = defineProps<{ 
     chron: Required<Chron>,
@@ -152,39 +153,6 @@ const daysTotal = computed(() =>{
     return differenceInCalendarDays(props.chron.end, props.chron.start);
 });
 
-const diameter = 400;
-const radius = diameter / 2;
-const origin = {
-    x: radius,
-    y: radius,
-}
-const q1 = `M ${origin.x},${origin.y} 
-              L ${origin.x + radius},${radius}
-              A ${radius},${radius}
-              0 0 0
-              ${origin.x}, ${origin.y - radius}
-              L ${origin.x},${origin.y}`;
-
-const q2 = `M ${origin.x},${origin.y} 
-    L ${origin.x},${origin.y - radius}
-    A ${radius},${radius}
-    0 0 0
-    ${origin.x - radius}, ${origin.y}
-    L ${origin.x},${origin.y}`;
-
-const q3 = `M ${origin.x},${origin.y} 
-    L ${origin.x - radius},${radius}
-    A ${radius},${radius}
-    0 0 0
-    ${origin.x}, ${origin.y + radius}
-    L ${origin.x},${origin.y}`;
-
-const q4 = `M ${origin.x},${origin.y} 
-    L ${origin.x},${origin.y + radius}
-    A ${radius},${radius}
-    0 0 0
-    ${origin.x + radius}, ${origin.y}
-    L ${origin.x},${origin.y}`;
 
 </script>
 
@@ -273,25 +241,7 @@ export default {
 
         <!-- donut style; refactor me to a component -->
         <div v-if="renderStyle==='donut'">
-            <h2>Donut style</h2>
-            <div>Coming soon!</div>
-            <svg :class="$style.donut" width="400" height="400">
-                
-                <!-- blue arc, cartesian (1, 0) to cartesian (0, 1)-->
-                 <path :d="q1" fill="blue"></path>
-
-                <!-- green arc, cartesian (0, 1) to cartesian (-1, 0)-->
-                <path :d="q2" fill="green"></path>
-
-                <!-- orange arc, cartesian (-1, 0) to cartesian (0, -1)-->
-                <path :d="q3" fill="orange"></path>
-
-                <!-- purple arc, cartesian (0, -1) to cartesian (0, 1)-->
-                <path :d="q4" fill="purple"></path>
-
-                <!-- circle, to create a donut effect -->
-                <circle :cx="radius" :cy="radius" :r="radius * 0.7" fill="white"></circle>
-            </svg>
+            <DonutDemo></DonutDemo>
         </div>
     </div>
 </template>
--- src/components/routes/DonutDemo.vue
+++ src/components/routes/DonutDemo.vue
@@ -1,5 +1,39 @@
 <script setup lang="ts">
 
+const diameter = 400;
+const radius = diameter / 2;
+const origin = {
+    x: radius,
+    y: radius,
+}
+const q1 = `M ${origin.x},${origin.y} 
+              L ${origin.x + radius},${radius}
+              A ${radius},${radius}
+              0 0 0
+              ${origin.x}, ${origin.y - radius}
+              L ${origin.x},${origin.y}`;
+
+const q2 = `M ${origin.x},${origin.y} 
+    L ${origin.x},${origin.y - radius}
+    A ${radius},${radius}
+    0 0 0
+    ${origin.x - radius}, ${origin.y}
+    L ${origin.x},${origin.y}`;
+
+const q3 = `M ${origin.x},${origin.y} 
+    L ${origin.x - radius},${radius}
+    A ${radius},${radius}
+    0 0 0
+    ${origin.x}, ${origin.y + radius}
+    L ${origin.x},${origin.y}`;
+
+const q4 = `M ${origin.x},${origin.y} 
+    L ${origin.x},${origin.y + radius}
+    A ${radius},${radius}
+    0 0 0
+    ${origin.x + radius}, ${origin.y}
+    L ${origin.x},${origin.y}`;
+
 
 </script>
 
@@ -11,8 +45,25 @@
 
 <template>
     <div :class="$style.donutDemo">
-        <h2>Donut</h2>
-        <p>Coming soon</p>
+        <h2>Donut style</h2>
+            <div>Coming soon!</div>
+            <svg :class="$style.donut" width="400" height="400">
+                
+                <!-- blue arc, cartesian (1, 0) to cartesian (0, 1)-->
+                 <path :d="q1" fill="blue"></path>
+
+                <!-- green arc, cartesian (0, 1) to cartesian (-1, 0)-->
+                <path :d="q2" fill="green"></path>
+
+                <!-- orange arc, cartesian (-1, 0) to cartesian (0, -1)-->
+                <path :d="q3" fill="orange"></path>
+
+                <!-- purple arc, cartesian (0, -1) to cartesian (0, 1)-->
+                <path :d="q4" fill="purple"></path>
+
+                <!-- circle, to create a donut effect -->
+                <circle :cx="radius" :cy="radius" :r="radius * 0.7" fill="white"></circle>
+            </svg>
     </div>
 </template>
 
