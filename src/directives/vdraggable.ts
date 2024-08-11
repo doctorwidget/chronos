69b97d6c435c01bd34012d5b9ce46fda690df0c0
Scott Fitz <doctorwidget@gmail.com>
2024-08-11 12:04:22 -0700

    use a mutationObserver to allow safe interaction via translation attributes

--- src/directives/vdraggable.ts
+++ src/directives/vdraggable.ts
@@ -19,24 +19,70 @@ export const dragAttrs = {
     transform: 'transform',
 };
 
-// TODO: create a JS Map<Element, Bindings>,
-// so we can do things like removing MutationObservers when we're done
-// (right now each of the lifecycle methods is a closure of its own,
-// so there's no good way to for the unmounted() method to remove
-// the mutationObservers set up by the mounted() method).
+// Shared map of elements to MutationObservers,
+// so we can clean up our mutation observers when the element unmounts.
+const observers = new Map<Element, MutationObserver>();
+
+/**
+ * Set up a mutation observer to watch for changes to dragAttrs.x and dragAttrs.y
+ * @param { Element} el - the element to observe
+ */
+export const observe = (el:Element) => {
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList: Array<MutationRecord>, _:MutationObserver) => {
+        for (const mutation of mutationList) {
+            if (mutation.type === "attributes") {
+                const name = mutation.attributeName;
+                if (name === dragAttrs.x || name === dragAttrs.y){
+                    console.log(`vDraggable: the ${name} attribute was modified.`);
+                    updateTranslation(el);
+                }
+            }
+        }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(el, config);
+
+    // record the observer so we can disconnect it later on
+    observers.set(el, observer);
+};
+
+/**
+ * Clean up an observer
+ * @param { Element } el - the element to unobserve
+ */
+export const unobserve = (el: Element) => {
+    if (observers.has(el)){
+        const observer = observers.get(el);
+        observer?.disconnect();
+        observers.delete(el);
+    }
+};
 
 /**
  * Translate an element by the supplied deltas.
  * Keeps track of previous shifts via drag attributes
+ * @param { Element } el - the element whose [transform:translate] value is to be mutated
+ * @param { number } deltaX - horizontal shift, i.e. change in the x coordinate
+ * @param { number } deltaY - vertical shift, i.e. change in the y coordinate 
  */
 export const translateBy = (el:Element, deltaX: number, deltaY: number) => {
     const baseX = Number(el.getAttribute(dragAttrs.x) || '') || 0; 
     const baseY = Number(el.getAttribute(dragAttrs.y) || '') || 0;
 
+    // calculate the final changed x and y
     const x = baseX + deltaX;
     const y = baseY + deltaY;
 
-    // remember the new baseX and baseY
+    // remember those new values
     el.setAttribute(dragAttrs.x, `${x}`);
     el.setAttribute(dragAttrs.y, `${y}`);
 
@@ -46,6 +92,7 @@ export const translateBy = (el:Element, deltaX: number, deltaY: number) => {
 
 /**
  * Reset the translation back to 0,0
+ * @param { Element } el - the element whose [transform:translate] value is to be reset
  */
 export const resetTranslation = (el:Element) => {
     el.setAttribute(dragAttrs.x, '0');
@@ -56,6 +103,7 @@ export const resetTranslation = (el:Element) => {
 
 /**
  * Set a new translation value based on [data-translate-x] and [data-translate-y]
+ * @param { Element } el - the svg element whose [transform:translate] value is to be updated
  */
 export const updateTranslation = (el: Element) => {
     const x = el.getAttribute(dragAttrs.x) || 0;
@@ -101,7 +149,7 @@ const mounted = (el:Element, binding:DirectiveBinding, node:VNode) => {
 
     // add a mutation observer, so that anyone has access to both
     // [data-translate-x] and [data-translate-y]
-
+    observe(el);
 };
 
 /**
@@ -116,6 +164,16 @@ const updated = (el:Element, binding:DirectiveBinding, node:VNode) => {
     resetTranslation(el);
 };
 
+/**
+ * Handler for when the element unmounts
+ * @param { Element } el - the svg element to be dragged
+ */ 
+const unmounted = (el:Element) => {
+    console.log(`vDraggable UNMOUNTED `, el);
+
+    unobserve(el);
+};
+
 
 /**
  * Custom directive to make an SVG draggable.
@@ -123,4 +181,5 @@ const updated = (el:Element, binding:DirectiveBinding, node:VNode) => {
 export const vDraggable: ObjectDirective = {
     mounted,
     updated,
+    unmounted,
 };
